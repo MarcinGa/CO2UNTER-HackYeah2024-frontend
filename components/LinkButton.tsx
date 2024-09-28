2bd6407b9bb03274ec3e9e83e11792455f59ddf7
Tomasz Kumiega <tomasz.kumiega@aldi-techhub.pl>
2024-09-29 00:38:59 +0200

    wip: link button

---
+++ components/LinkButton.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from 'expo-router';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+export default function LinkButton({ route: }) {
+  const router = useRouter();
+
+  return (
+      <TouchableOpacity style={styles.button} onPress={() => router.push(route)}>
+        <Text style={styles.buttonText}>Przejdź do strony "O nas"</Text>
+      </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
