016a016e193243bba64103d952e01233ccc43541
MarcinGa <marcin.gasiorek91@gmail.com>
2024-09-28 21:26:07 +0200

    poprawa text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
