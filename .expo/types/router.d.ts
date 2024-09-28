9ecb42ae7f64f88daf64c5a9e51c0f0d25cae2dd
Tomasz Kumiega <tomasz.kumiega@aldi-techhub.pl>
2024-09-29 00:38:06 +0200

    feat: route & async storage

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/..\components\forms\ConsumptionProductsForm` | `/..\components\forms\HomeEnergyForm` | `/..\components\forms\TransportForm` | `/..\components\parks\PocketParks` | `/..\components\parks\TownParks` | `/_sitemap` | `/convert` | `/explore` | `/individual` | `/parks` | `/parks/pocket-park-search` | `/parks/town-park-search` | `/services` | `/transport`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/..\components\LinkButton` | `/_sitemap` | `/convert` | `/explore` | `/individual` | `/parks` | `/parks/pocket-park-search` | `/parks/town-park-search` | `/services` | `/transport`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
