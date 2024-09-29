0806c7cd049282b18ce923e1ae5914c26d3712fe
Tomasz Kumiega <tomasz.kumiega@aldi-techhub.pl>
2024-09-29 03:21:14 +0200

    add rnr

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -3,6 +3,9 @@ import { useColorScheme as useNativewindColorScheme } from "nativewind";
 export function useColorScheme() {
   const { colorScheme, setColorScheme, toggleColorScheme } =
     useNativewindColorScheme();
+
+  alert("1");
+
   return {
     colorScheme: colorScheme ?? "dark",
     isDarkColorScheme: colorScheme === "dark",
